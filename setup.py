c5540d93e2d821864eac2b3e1a35a691456819ce
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-07-31 12:14:57 +1000

    Initial import.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from distutils.core import setup
+
+setup(name='EOtools',
+      version='0.1',
+      package_dir={'EOtools': 'src'},
+      packages=['EOtools', 'EOtools.DatasetDrivers', 'EOtools.stats'],
+      author='Roger Edberg, Alex Ip, Josh Sixsmith',
+      maintainer='Geoscience Australia',
+      description='Collection of Earth Observation Tools.',
+      long_description='Collection includes: DatasetDrivers, temporal statistics, array tiling.',
+      license='BSD 3',
+      )
