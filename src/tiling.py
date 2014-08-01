5720236435e9d488a6e627917a30ed6e5500bfa1
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-08-01 11:52:51 +1000

    Added use example to documentation.

--- src/tiling.py
+++ src/tiling.py
@@ -26,6 +26,28 @@ def generate_tiles(samples, lines, xtile=100, ytile=100):
         A list of tuples containing the precalculated tiles used for
         indexing a larger array.
         Each tuple contains (ystart,yend,xstart,xend).
+
+    Example:
+
+        >>> tiles = generate_tiles(8624, 7567, xtile=1000,ytile=400)
+        >>>
+        >>> for tile in tiles:
+        >>>     ystart = int(tile[0])
+        >>>     yend   = int(tile[1])
+        >>>     xstart = int(tile[2])
+        >>>     xend   = int(tile[3])
+        >>>     xsize  = int(xend - xstart)
+        >>>     ysize  = int(yend - ystart)
+        >>>
+        >>>     # When used to read data from disk
+        >>>     subset = gdal_indataset.ReadAsArray(xstart, ystart, xsize, ysize)
+        >>>
+        >>>     # The same method can be used to write to disk.
+        >>>     gdal_outdataset.WriteArray(array, xstart, ystart)
+        >>>
+        >>>     # Or simply move the tile window across an array
+        >>>     subset = array[ystart:yend,xstart:xend] # 2D
+        >>>     subset = array[:,ystart:yend,xstart:xend] # 3D
     """
     ncols = samples
     nrows = lines
