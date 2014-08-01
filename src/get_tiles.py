88849d15045c226b687c75ad8c5b8558796f5ee1
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-08-01 11:37:08 +1000

    Function namespace change & added documentation.

--- src/get_tiles.py
+++ src/get_tiles.py
@@ -2,65 +2,29 @@ import numpy
 
 #Author: Josh Sixsmith; joshua.sixsmith@ga.gov.au
 
-def get_tile(array, xtile=100,ytile=100):
-    st = datetime.datetime.now()
-    dims = array.shape
-    ncols = dims[1]
-    nrows = dims[0]
-    if len(dims) >2:
-        ncols = dims[2]
-        nrows = dims[1]
-        dims  = (nrows,ncols)
-    xstart = numpy.arange(0,ncols,xtile)
-    ystart = numpy.arange(0,nrows,ytile)
-    xend   = numpy.zeros(xstart.shape, dtype='int32')
-    yend   = numpy.zeros(ystart.shape, dtype='int32')
-    i = 0
-    for step in xstart:
-        if step + xtile < ncols:
-            xend[i] = step + xtile
-        else:
-            xend[i] = ncols
-        i += 1
-    i = 0
-    for step in ystart:
-        if step + ytile < nrows:
-            yend[i] = step + ytile
-        else:
-            yend[i] = nrows
-        i += 1
-    et = datetime.datetime.now()
-    print 'get_tile time taken: ', et - st
-    return (xstart, xend), (ystart,yend)
+def generate_tiles(samples, lines, xtile=100, ytile=100):
+    """
+    Generates a list of tile indices for a 2D array.
 
-def get_tile2(array, xtile=100,ytile=100):
-    st = datetime.datetime.now()
-    dims = array.shape
-    ncols = dims[1]
-    nrows = dims[0]
-    l = []
-    if len(dims) >2:
-        ncols = dims[2]
-        nrows = dims[1]
-        dims  = (nrows,ncols)
-    xstart = numpy.arange(0,ncols,xtile)
-    ystart = numpy.arange(0,nrows,ytile)
-    for ystep in ystart:
-        if ystep + ytile < nrows:
-            yend = ystep + ytile
-        else:
-            yend = nrows
-        for xstep in xstart:
-            if xstep + xtile < ncols:
-                xend = xstep + xtile
-            else:
-                xend = ncols
-            l.append((ystep,yend,xstep, xend))
-    et = datetime.datetime.now()
-    print 'get_tile2 time taken: ', et - st
-    return l 
+    :param samples:
+        An integer expressing the total number of samples in an array.
 
-def get_tile3(samples, lines, xtile=100,ytile=100):
+    :param lines:
+        An integer expressing the total number of lines in an array.
+
+    :param xtile:
+        (Optional) The desired size of the tile in the x-direction.
+        Default is 100.
+
+    :param ytile:
+        (Optional) The desired size of the tile in the y-direction.
+        Default is 100.
+
+    :return:
+        A list of tuples containing the precalculated tiles used for
+        indexing a larger array.
+        Each tuple contains (ystart,yend,xstart,xend).
+    """
     ncols = samples
     nrows = lines
     tiles = []
