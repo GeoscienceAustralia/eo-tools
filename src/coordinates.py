9d0fb8e5d6012181df37ed0a96799bd5b200c623
Josh Sixsmith <josh.sixsmith@gmail.com>
2015-01-28 00:01:46 +1100

    Bug fix.

--- src/coordinates.py
+++ src/coordinates.py
@@ -52,8 +52,10 @@ def convert_coordinates(geotransform, xy, to_map=True, centre=False):
                 points.append((x, y))
         else:
             invgt = gdal.InvGeoTransform(geotransform)[1]
-            x = int(xy[0] * invgt[1] + invgt[0])
-            y = int(invgt[3] - xy[1] * abs(invgt[5]))
+            for point in xy:
+                x = int(point[0] * invgt[1] + invgt[0])
+                y = int(invgt[3] - point[1] * abs(invgt[5]))
+                points.append((x, y))
         return points
     else:
         if to_map:
@@ -104,4 +106,3 @@ def transform_coordinates(xy, to_crs):
     else:
         (x, y, _) = transformation.TransformPoint(xy[0], xy[1])
         return (x, y)
-
