2ce670df5301638a15b420503cba4a2d146f7c6e
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-05-30 22:29:47 +1000

    Added projection checks to spatial_intersection. Added retrieve attribute table function.

--- eotools/vector.py
+++ eotools/vector.py
@@ -3,6 +3,9 @@
 from __future__ import absolute_import
 import numpy
 import ogr
+import pandas
+
+from image_processing.segmentation.rasterise import project_vector
 
 
 def spatial_intersection(input_vector_fname, base_vector_fname, envelope=True):
@@ -32,6 +35,13 @@ def spatial_intersection(input_vector_fname, base_vector_fname, envelope=True):
     lyr1 = vec_ds1.GetLayer()
     lyr2 = vec_ds2.GetLayer()
 
+    prj1 = lyr1.GetSpatialRef()
+    prj2 = lyr2.GetSpatialRef()
+
+    # Transfrom the geometry as needed
+    if not prj1.IsSame(prj2):
+        project_vector(lyr1, prj1, prj2)
+
     fids = []
 
     if envelope:
@@ -53,3 +63,33 @@ def spatial_intersection(input_vector_fname, base_vector_fname, envelope=True):
     fids = numpy.unique(numpy.array(fids)).tolist()
 
     return fids
+
+
+def retrieve_attribute_table(layer):
+    """
+    Retrieves the attribute table for the input vector layer.
+
+    :param layer:
+        An ogr `Layer` object.
+
+    :return:
+        A `pandas.DataFrame` containing the attribute table.
+    """
+    defn = layer.GetLayerDefn()
+    cols = []
+    cols.append('FID')
+
+    for i in range(defn.GetFieldCount()):
+        name = defn.GetFieldDefn(i).GetName()
+        cols.append(name)
+
+    df = pandas.DataFrame(columns=cols)
+    table  {}
+
+    for feat in layer:
+        table['FID'] = feat.GetFID()
+        for key in feat.keys():
+            table[key] = feat.GetField(key)
+        df = df.append(table, ignore_index=True)
+
+    return df
