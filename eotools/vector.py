e296705c501509369fbfd486fce5fa99ff38f914
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2016-01-21 14:11:07 +1100

    Made use of geopandas to perform spatial intersections.

--- eotools/vector.py
+++ eotools/vector.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 from __future__ import absolute_import
-import numpy
-import ogr
-import pandas
 
-from image_processing.segmentation.rasterise import project_vector
+import geopandas
+import numpy
+from rasterio.crs import is_same_crs
 
 
 def spatial_intersection(base_vector_fname, input_vector_fname, envelope=True):
@@ -30,66 +29,40 @@ def spatial_intersection(base_vector_fname, input_vector_fname, envelope=True):
     :return:
         A `list` containing the FID's of the base vector.
     """
-    vec_ds1 = ogr.Open(input_vector_fname)
-    vec_ds2 = ogr.Open(base_vector_fname)
-    lyr1 = vec_ds1.GetLayer()
-    lyr2 = vec_ds2.GetLayer()
+    base_df =  geopandas.read_file(base_vector_fname)
+    input_df = geopandas.read_file(input_vector_fname)
 
-    prj1 = lyr1.GetSpatialRef()
-    prj2 = lyr2.GetSpatialRef()
+    if not is_same_crs(base_df.crs, input_df.crs):
+        input_df.to_crs(base_df.crs, inplace=True)
 
-    # Transfrom the geometry as needed
-    if not prj1.IsSame(prj2):
-        project_vector(lyr1, prj1, prj2)
+    wh = numpy.zeros((base_df.shape[0]), dtype='bool')
 
-    fids = []
-
+    # Not the most optimal solution, i.e. libspatialindex
+    # but geopandas is quick enough just to loop over every feature
     if envelope:
-        for feat2 in lyr2:
-            geom = feat2.GetGeometryRef()
-            xmin, xmax, ymin, ymax = geom.GetEnvelope()
-            lyr1.SetSpatialFilterRect(xmin, ymin, xmax, ymax)
-            for feat1 in lyr1:
-                fids.append(feat1.GetFID())
-            lyr1.SetSpatialFilter(None)
+        for feat in input_df.geometry:
+            wh |= dcdf.intersects(feat.envelope):
+            fids = base_df[wh].index.values.tolist()
     else:
-        for feat2 in lyr2:
-            ref = feat2.GetGeometryRef()
-            lyr1.SetSpatialFilter(ref)
-            for feat1 in lyr1:
-                fids.append(feat1.GetFID())
-            lyr1.SetSpatialFilter(None)
-
-    fids = numpy.unique(numpy.array(fids)).tolist()
+        for feat in input_df.geometry:
+            wh |= dcdf.intersects(feat):
+            fids = base_df[wh].index.values.tolist()
 
     return fids
 
 
-def retrieve_attribute_table(layer):
+def retrieve_attribute_table(vector_fname):
     """
-    Retrieves the attribute table for the input vector layer.
+    Retrieves the attribute table for the input vector file.
 
-    :param layer:
-        An ogr `Layer` object.
+    :param vector_fname:
+        A string containing the full file path name to an
+        OGR compliant vector file.
 
     :return:
         A `pandas.DataFrame` containing the attribute table.
     """
-    defn = layer.GetLayerDefn()
-    cols = []
-    cols.append('FID')
-
-    for i in range(defn.GetFieldCount()):
-        name = defn.GetFieldDefn(i).GetName()
-        cols.append(name)
-
-    df = pandas.DataFrame(columns=cols)
-    table = {}
-
-    for feat in layer:
-        table['FID'] = feat.GetFID()
-        for key in feat.keys():
-            table[key] = feat.GetField(key)
-        df = df.append(table, ignore_index=True)
+    df = geopandas.read_file(vector_fname)
+    df.drop('geometry', axis=1, inplace=True)
 
     return df
